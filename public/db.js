20fd3cdc6e95ea9c0ec547e36f83b998e319db24
Cory Piette <coryjpiette@gmail.com>
2021-10-02 16:07:18 -0400

    formatting

--- public/db.js
+++ public/db.js
@@ -36,31 +36,31 @@ request.onsuccess = (event) => {
 
     if (navigator.onLine) {
         checkDatabase();
-      }
+    }
 
-    };
+};
 
-        request.onerror = function(event) {
-        console.log("Please try again! " + event.target.errorCode);
-      };
+request.onerror = function (event) {
+    console.log("Please try again! " + event.target.errorCode);
+};
 
-      function saveRecord(record) {
-        const transaction = db.transaction(["pending"], "readwrite");
-        const store = transaction.objectStore("pending");
-        store.add(record);
-      }
+function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+}
 
-      function checkDatabase() {
-        const transaction = db.transaction(["pending"], "readwrite");
-           const store = transaction.objectStore("pending");
-        const getAll = store.getAll();
-        getAll.onsuccess = function() {
-          if (getAll.result.length > 0) {
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+    getAll.onsuccess = function () {
+        if (getAll.result.length > 0) {
             fetch("/api/transaction/bulk", {
-              method: "POST",
-              body: JSON.stringify(getAll.result),
-                  headers: {
-                Accept: "application/json, text/plain, */*",
-                "Content-Type": "application/json"
-              }
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
             })
