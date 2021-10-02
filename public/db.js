ee2ee2578412b2663f3071d3735389fcd0ca5da5
Cory Piette <coryjpiette@gmail.com>
2021-10-02 09:01:59 -0400

    defining DB consts and starting requests

--- public/db.js
+++ public/db.js
@@ -1,10 +1,32 @@
 
-const indexedDB = 
+const indexedDB =
 
-window.indexedDB ||
-window.msIndexedDB ||
-window.mozIndexedDB ||
-window.shimIndexedDB ||
-window.webkitIndexedDB
-;
+    window.indexedDB ||
+    window.msIndexedDB ||
+    window.mozIndexedDB ||
+    window.shimIndexedDB ||
+    window.webkitIndexedDB
+    ;
 
+window.IDBTransaction =
+
+    window.IDBTransaction ||
+    window.webkitIDBTransaction ||
+    window.msIDBTransaction ||
+    { READ_WRITE: "readwrite" };
+
+
+window.IDBKeyRange =
+
+    window.IDBKeyRange ||
+    window.webkitIDBKeyRange ||
+    window.msIDBKeyRange;
+
+
+ let db;
+    const request = window.indexedDB.open("budget", 1);
+       request.onupgradeneeded = (event) => {
+           
+     const db = event.result;
+       db.createObjectStore("pending", { autoIncrement: true });
+    };
