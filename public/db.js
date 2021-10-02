dd886552dcaa2715a1b870def8d6d9d67b3b3d48
Cory Piette <coryjpiette@gmail.com>
2021-10-02 09:19:25 -0400

    adding worker.js file and adding files to cache

--- public/db.js
+++ public/db.js
@@ -23,10 +23,23 @@ window.IDBKeyRange =
     window.msIDBKeyRange;
 
 
- let db;
-    const request = window.indexedDB.open("budget", 1);
-       request.onupgradeneeded = (event) => {
-           
-     const db = event.result;
-       db.createObjectStore("pending", { autoIncrement: true });
+let db;
+const request = window.indexedDB.open("budget", 1);
+request.onupgradeneeded = (event) => {
+
+    const db = event.result;
+    db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = (event) => {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        checkDatabase();
+      }
+
     };
+
+    request.onerror = function(event) {
+        console.log("Please try again! " + event.target.errorCode);
+      };
---
+++ public/worker.js
@@ -0,0 +1,13 @@
+
+
+
+const FILES_TO_CACHE = [
+    '/',
+    '/index.js',
+    '/index.html',
+    '/db.js',
+    '/worker.js',
+    '/styles.css',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+  ];
