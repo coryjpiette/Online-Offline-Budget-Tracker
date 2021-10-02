eeebee6e915081b7901386ac7d7cf75391fd79aa
Cory Piette <coryjpiette@gmail.com>
2021-10-02 16:05:39 -0400

    adding database check and api fetch

--- public/db.js
+++ public/db.js
@@ -40,6 +40,27 @@ request.onsuccess = (event) => {
 
     };
 
-    request.onerror = function(event) {
+        request.onerror = function(event) {
         console.log("Please try again! " + event.target.errorCode);
       };
+
+      function saveRecord(record) {
+        const transaction = db.transaction(["pending"], "readwrite");
+        const store = transaction.objectStore("pending");
+        store.add(record);
+      }
+
+      function checkDatabase() {
+        const transaction = db.transaction(["pending"], "readwrite");
+           const store = transaction.objectStore("pending");
+        const getAll = store.getAll();
+        getAll.onsuccess = function() {
+          if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+              method: "POST",
+              body: JSON.stringify(getAll.result),
+                  headers: {
+                Accept: "application/json, text/plain, */*",
+                "Content-Type": "application/json"
+              }
+            })
