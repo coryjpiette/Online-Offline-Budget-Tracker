17005b4cc78dc732977b5366c61738fa1371aa26
Cory Piette <coryjpiette@gmail.com>
2021-10-04 07:15:20 -0400

    fixing const PORT

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,7 +6,7 @@ const FILES_TO_CACHE = [
     '/index.js',
     '/index.html',
     '/db.js',
-    '/worker.js',
+    '/service-worker.js',
     '/styles.css',
     '/icons/icon-192x192.png',
     '/icons/icon-512x512.png'
